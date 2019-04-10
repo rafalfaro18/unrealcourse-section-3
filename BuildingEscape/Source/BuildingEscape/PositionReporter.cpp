fc26005638eae2d6f19c42d3a4ac7858f4a4967b
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-04-09 19:33:52 -0600

    Adds library for Actor methods autocomplete

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReporter.h"
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
