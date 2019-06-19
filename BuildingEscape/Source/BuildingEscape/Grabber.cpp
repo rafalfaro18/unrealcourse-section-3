50630b86a71d9246b14cbd7c95cb6953c25a65e3
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-06-18 21:59:28 -0600

    Adds missing physics handle component error log

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -25,6 +25,17 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!") );
+
+	/// Loof for attached Physics Handle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsHandle) {
+		// Physics handle is found
+	}
+	else {
+		// Log at error verbosity if no component found
+		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName());
+	}
 	
 }
 
