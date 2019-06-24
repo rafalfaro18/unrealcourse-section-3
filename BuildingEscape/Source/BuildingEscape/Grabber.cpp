4dad8e929d4cc266a1cb55bebe6e2aaf1e6a6ec8
Rafael Alfaro <rafalfaro_18@outlook.com>
2019-06-24 07:28:29 -0600

    Protect pointers

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -61,6 +61,7 @@ void UGrabber::Grab() {
 
 	/// If we hit something then attach a physics handle
 	if (ActorHit) {
+		if (!PhysicsHandle) { return; }
 		PhysicsHandle->GrabComponentAtLocationWithRotation(
 			ComponentToGrab,
 			NAME_None, // no bones needed
@@ -71,6 +72,7 @@ void UGrabber::Grab() {
 }
 
 void UGrabber::Release() {
+	if (!PhysicsHandle) { return; }
 	PhysicsHandle->ReleaseComponent();
 }
 
@@ -79,6 +81,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	if (!PhysicsHandle) { return; }
 	// if the physics handle us attached
 	if (PhysicsHandle->GrabbedComponent) {
 		// move the object that we're holding
